f072a2445ebd86fe92a912278d74becb6e54a9f4
MGSimard <mgsimard.dev@gmail.com>
2025-07-10 06:26:52 -0400

    Convex query & mutate tests

--- convex/tasks.ts
+++ convex/tasks.ts
@@ -1,8 +1,19 @@
-import { query } from "./_generated/server";
+import { mutation, query } from "./_generated/server";
+import { v } from "convex/values";
 
 export const get = query({
   args: {},
   handler: async (ctx) => {
     return await ctx.db.query("tasks").collect();
+  },
+});
+
+export const add = mutation({
+  args: {
+    text: v.string(),
+  },
+  returns: v.id("tasks"),
+  handler: async (ctx, args) => {
+    return await ctx.db.insert("tasks", { text: args.text, isCompleted: false });
   },
 });

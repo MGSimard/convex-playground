273bd494c97c452744a2503b845072efe2537728
MGSimard <mgsimard.dev@gmail.com>
2025-07-10 21:40:42 -0400

    a

--- src/routes/sync/route.tsx
+++ src/routes/sync/route.tsx
@@ -1,5 +1,6 @@
 import { createFileRoute, Outlet } from "@tanstack/react-router";
 import { ExampleCombobox } from "@/_components/kanban/Combobox";
+import { Separator } from "@/_components/ui/separator";
 
 export const Route = createFileRoute("/sync")({
   component: RouteComponent,
@@ -8,7 +9,10 @@ export const Route = createFileRoute("/sync")({
 function RouteComponent() {
   return (
     <div className="flex-grow w-full h-full p-6">
-      <ExampleCombobox />
+      <header>
+        <ExampleCombobox />
+      </header>
+      <Separator className="my-6" />
       <Outlet />
     </div>
   );

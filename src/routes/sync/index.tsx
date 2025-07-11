d401487e9342e551a9ffa2fb0a9b80638ce86c23
MGSimard <mgsimard.dev@gmail.com>
2025-07-10 21:01:58 -0400

    Sync plan

--- src/routes/sync/index.tsx
+++ src/routes/sync/index.tsx
@@ -5,6 +5,15 @@ export const Route = createFileRoute("/sync/")({
   loader: () => ({ crumb: "Sync" }),
 });
 
+// SYNC PLAN (Kanban boards)
+// Boards: 1 Row, infinite columns, horizontal scroll on overflow
+// Lists (Columns): Draggable, Header title (click to rename), table options button, Cards, + Add Card (footer)
+// Add card, copy list, move list, move all cards
+// Cards (Rows): Draggable, Edit, Embed preview, text, links (Title -> Desc -> Full link)
+// Adding a card from bottom shows the input at bottom of the list
+// Adding a card from top right control shows input at top of the list
+// Card attribution, labels, move, start date & due date, reminders
+
 function RouteComponent() {
-  return <div>Hello "/sync/"!</div>;
+  return <div className="flex-grow w-full h-full p-6">Hello "/sync/"!</div>;
 }

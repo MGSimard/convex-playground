2ef22164838f84a4c9b9f509cc96cdb706f1c6d1
MGSimard <mgsimard.dev@gmail.com>
2025-07-14 13:46:56 -0400

    Draggable

--- src/_components/kanban/Card.tsx
+++ src/_components/kanban/Card.tsx
@@ -1,6 +1,7 @@
 import { SquarePen } from "lucide-react";
 import type { Doc } from "../../../convex/_generated/dataModel";
 import { Button } from "@/_components/ui/button";
+import { useDraggable } from "@dnd-kit/react";
 
 interface CardProps {
   card: Doc<"cards">;
@@ -9,8 +10,14 @@ interface CardProps {
 // Handle accessible interactivity once we implement dndkit+
 
 export function Card({ card }: CardProps) {
+  const { ref } = useDraggable({
+    id: card._id,
+  });
+
   return (
-    <li className="relative text-sm text-muted-foreground bg-muted rounded-md px-3 py-1.5 overflow-hidden break-all group hover:outline-1 hover:outline-primary">
+    <li
+      ref={ref}
+      className="relative text-sm text-muted-foreground bg-muted rounded-md px-3 py-1.5 overflow-hidden break-all group hover:outline-1 hover:outline-primary">
       {card.content}
       <Button
         variant="ghost"
